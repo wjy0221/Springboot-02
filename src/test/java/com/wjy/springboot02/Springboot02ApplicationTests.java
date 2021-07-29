fa76ee424dd9bcbe7e23b043e0fa378445664693
wjy0221 <1051126553@qq.com>
2021-07-29 20:42:22 +0800

    springboot

---
+++ src/test/java/com/wjy/springboot02/Springboot02ApplicationTests.java
@@ -0,0 +1,19 @@
+package com.wjy.springboot02;
+
+import com.wjy.springboot02.pojo.Person;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot02ApplicationTests {
+
+    //@Autowired
+   // private Person person;
+
+    @Test
+    void contextLoads() {
+       // System.out.println(person);
+    }
+
+}
